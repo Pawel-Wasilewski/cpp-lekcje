2a90aac726d4e3c0d7fa3d52510a76bf07304708
Paweł Wasilewski <99391325+Widznajomych@users.noreply.github.com>
2023-09-20 09:55:39 +0200

    Add files via upload

---
+++ skrypt_trojkat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+
+if (podstawa > bok+bok2) {
+    cout << "podstawa jest za dluga by twe wymiary jakie podales bylby trojkatem";
+    return 0;
+}
+
+*/
+int podstawa {
+    
+}
+int main () {
+
+}
