7620fadf53cefb69fa4e4cace57a5400b35cee3f
Paweł Wasilewski <99391325+Widznajomych@users.noreply.github.com>
2023-10-04 09:08:00 +0200

    Update skrypt_trojkat.cpp

--- skrypt_trojkat.cpp
+++ skrypt_trojkat.cpp
@@ -34,8 +34,8 @@ int main() {
     if ((bok1^2) + (bok2^2) == (podstawa^2)) {
         cout << "To trojkat prostokatny" << endl;
     }
-    int liczeniep = 0.5 * (podstawa + bok2 + bok3);
-    int trokatap = sqrt(liczeniep * (liczeniep - podstawa) * (liczeniep - bok2) * (liczeniep - bok3));
+    int liczeniep = 0.5 * (podstawa + bok2 + bok1);
+    int trokatap = sqrt(liczeniep * (liczeniep - podstawa) * (liczeniep - bok2) * (liczeniep - bok1));
     int obwtrojkota = podstawa + bok1 + bok2;
     cout << "Obwód trójkąta wynosi " << obwtrojkota << endl;
     cout << "Pole trójkąta wynosi " << trokatap << endl;
