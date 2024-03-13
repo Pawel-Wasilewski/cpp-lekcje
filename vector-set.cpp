f2976792727d0080113242aa03440904745fd28f
palstr <144432428+palstr@users.noreply.github.com>
2024-03-13 08:50:35 +0100

    Update vector-set.cpp

--- vector-set.cpp
+++ vector-set.cpp
@@ -1,2 +1,54 @@
 //Napisz program który pobiera od użytkownika liczbę całkowitą a następnie n liczb całkowitych i zapisuje je do wektora. 
 //Następnie program powinien usunąć z wektora wszystkie duplikaty, zachowując jedynie unikalne elementy. Na końcu program powinien wyświetlić unikalne elementy w porządku ich wystąpienia. 
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <set>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+    int a;
+    int n;
+    
+    cout << "Podaj liczbe calkowita: ";
+    cin >> a;
+    
+    vector<int> wektor = {a};
+    
+    cout << "Ile jeszcze chcesz liczb? ";
+    cin >> n;
+    
+   srand(static_cast<unsigned>(time(NULL)));
+    
+     cout << "Wektor przed usunieciem duplikatow: " << endl;
+    
+     for(int i = 0; i < n; i++){
+     wektor.push_back(rand() % 10);
+     }
+     
+    for (int d : wektor)
+    {
+        cout << d << " ";
+    }
+    
+
+     cout <<"." << endl;
+     
+
+     set<int> uniqwe;
+
+      for(int c : wektor){
+          uniqwe.insert(c);
+      }
+
+      cout << "Wektor po sortowaniu oraz usunieciu duplikatow: " << endl;
+
+      for(int b : uniqwe){
+          cout << b << " ";
+      }
+      
+ }
+
