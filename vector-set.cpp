9dd4eac0247b2f8a3a5b9265c653de54930a0764
palstr <144432428+palstr@users.noreply.github.com>
2024-03-13 07:37:20 +0100

    Create vector-set.cpp

---
+++ vector-set.cpp
@@ -0,0 +1,2 @@
+Napisz program który pobiera od użytkownika liczbę całkowitą a następnie n liczb całkowitych i zapisuje je do wektora. 
+Następnie program powinien usunąć z wektora wszystkie duplikaty, zachowując jedynie unikalne elementy. Na końcu program powinien wyświetlić unikalne elementy w porządku ich wystąpienia. 
