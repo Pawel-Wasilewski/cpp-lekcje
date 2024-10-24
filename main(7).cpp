e00032573683d8160fdf9bd0f45f55c9b624af37
Paweł Wasilewski <99391325+Pawel-Wasilewski@users.noreply.github.com>
2024-10-24 13:15:44 +0200

    Update main(7).cpp

--- main(7).cpp
+++ main(7).cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include <cmath>
 #include <string>
 
 class Punkt2w {
     private:
-    int x, y, wx, wy; // Do układu
-    int mo = 0;
+    int x, y, w1, w2;
+    int mo;
 
     public:
     Punkt2w() {
-        int x, y, wx, wy;
-        int mo = 0;
-
+        x = y = w1 = w2 = 0;
+        mo = 0;
         std::cout << "Zainicjowano obiekt na podstawie klasy Punkt2w" << std::endl;
     }
 
@@ -21,60 +19,89 @@ class Punkt2w {
         std::cout << "y: ";
         std::cin >> y;
     }
+
     void wczytaj() {
         std::cout << "x: " << x << std::endl << "y: " << y << std::endl;
     }
 
-    void wektor2w() {
-        if (x < mo) {
-            wx = x * -1;
-        } 
-        else {
-            wx = x;
-        }
-        
-        if (y < mo) {
-            wy = y;
-        } 
-        else {
-            wy = y * -1;
-        }
-        
-        std::cout << "Wektor do miejsca zerowego: " << wx << ';' << wy << std::endl;
+    void wektor2w(int arg1, int arg2) {
+        w1 = arg1 * -1;
+        w2 = arg2 * -1;
+        std::cout << "Wektor do miejsca zerowego: " << w1 << ';' << w2 << std::endl;
     }
 
     int getx() {
         return x;
     }
+
     int gety() {
         return y;
     }
 
     ~Punkt2w() {
-        std::cout << "Obiekt na podstawie Punkt2w został zniszczony";
+        std::cout << "Obiekt na podstawie Punkt2w został zniszczony" << std::endl;
     }
 };
+
 class Punkt3w {
     private:
-    int xz,yz;
-    int z;
+    int x, y, z;
 
     public:
-
     Punkt3w() {
-        this->xz =
+        x = y = z = 0;
+        std::cout << "Zainicjowano obiekt na podstawie klasy Punkt3w" << std::endl;
     }
 
+    void wypisz() {
+        std::cout << "Wypisz: " << std::endl << "x: ";
+        std::cin >> x;
+        std::cout << "y: ";
+        std::cin >> y;
+        std::cout << "z: ";
+        std::cin >> z;
+    }
+
+    void wczytaj() {
+        std::cout << "x: " << x << std::endl << "y: " << y << std::endl << "z: " << z << std::endl;
+    }
+
+    void rzutuj() {
+        int wyb;
+        std::cout << "którego potrzebujesz: " << std::endl
+                  << "xz (1)" << std::endl
+                  << "xy (2)" << std::endl
+                  << "yz (3)" << std::endl;
+        std::cin >> wyb;
+        if (wyb == 1) {
+            Punkt2w p;
+            p.wektor2w(x, z);
+        } else if (wyb == 2) {
+            Punkt2w p;
+            p.wektor2w(x, y);
+        } else if (wyb == 3) {
+            Punkt2w p;
+            p.wektor2w(y, z);
+        } else {
+            std::cout << "zle wpisane" << std::endl;
+        }
+    }
+
+    ~Punkt3w() {
+        std::cout << "Obiekt na podstawie Punkt3w został zniszczony" << std::endl;
+    }
 };
-
-
 
 int main() {
     Punkt2w p1;
-
     p1.wczytaj();
     p1.wypisz();
-    p1.wektor2w();
+    p1.wektor2w(p1.getx(), p1.gety());
+
+    Punkt3w p2;
+    p2.wczytaj();
+    p2.wypisz();
+    p2.rzutuj();
 
     return 0;
 }
