5bac0a05a65a29b3a5c72f077473e39efeef7175
palstr <144432428+palstr@users.noreply.github.com>
2025-03-27 13:11:25 +0100

    Update ZZ2.3_kevinWYJATKI.cpp

--- ZZ2.3_kevinWYJATKI.cpp
+++ ZZ2.3_kevinWYJATKI.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
 
+using namespace std;
+
+void wczytajInt(int liczba1, int liczba2){
+    while(true){
+        try{
+            cout << "Podaj liczbę 1 -> ";
+            cin >> liczba1;
+    
+            if(cin.fail()){
+                cin.clear();
+                cin.ignore(1000,'\n');
+        
+                throw runtime_error("Postaraj się bardziej:");
+            }
+            
+            break;
+        }
+        catch(const exception& e){
+            cout << e.what() << endl;
+        }
+        
+    }
+    
+    while(true){
+        try{
+            cout << "Podaj liczbę 2 -> ";
+            cin >> liczba2;
+    
+            if(cin.fail()){
+                cin.clear();
+                cin.ignore(1000,'\n');
+        
+                throw runtime_error("Postaraj się bardziej:");
+            }
+            
+            break;
+        }
+        catch(const exception& e){
+            cout << e.what() << endl;
+        }
+        
+    }
+}
+
+int main()
+{
+    int liczba1, liczba2;
+    
+    wczytajInt(liczba1,liczba2);
+    
+    cout << "Wynik: " << liczba1+liczba2;
+
+    return 0;
+}
